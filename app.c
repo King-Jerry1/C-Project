8f424719c1656cc93bdfe2510dc4a9f07ec4e33b
King Jerry <104693905+King-Jerry1@users.noreply.github.com>
2024-09-05 11:34:55 +0100

    Intitial Commit

---
+++ app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num1 = 1;
+    int num2 = 3;
+    int total = num1 + num2;
+    printf("Total: %d\n", total); // Print the total
+    printf("Test and debug completed successfuly by Comr.Jeremiah"); //Success Message
+    return 0;
+}
+
